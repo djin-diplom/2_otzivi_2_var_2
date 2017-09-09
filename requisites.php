63edd7068c46c22fca013004dc1e35facdca2b49
21vek <git@github.com>
2017-09-09 20:24:54 +0300

    Initial commit

---
+++ requisites.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set('Europe/Moscow');
+$datetime_site = date("Y-m-d H:i:s");
+
+$main_name = "http://vivadiplom.ru";
+$donor_name = 'http://studotzyv.ru';
+$donor_site_name = 'Студ Отзыв';
+$donor_site_name_2 = 'Студ-Отзыв';
+$donor_site_name_3 = 'Cтуд-Отзыв';
+$donor_site_name_4 = 'add@studotzyv.ru';
+$site_name = "Вива Диплом";
+$site_name_2 = "add@vivadiplom.ru";
