a386baf172f3e2e8f875775eec5d9e92bca6b2e1
21vek <git@github.com>
2017-09-17 11:40:30 +0300

    1

--- requisites.php
+++ requisites.php
@@ -11,3 +11,20 @@ $donor_site_name_3 = 'Cтуд-Отзыв';
 $donor_site_name_4 = 'add@studotzyv.ru';
 $site_name = "Вива Диплом";
 $site_name_2 = "add@vivadiplom.ru";
+
+
+$raskrutka_site_1 = 'knowbaza.ru';
+$raskrutka_site_2 = 'knowbaza';
+$raskrutka_site_3 = 'KNOWBAZA.RU';
+$raskrutka_site_4 = 'knowbaza_ru';
+$raskrutka_site_5 = 'KNOWBAZA';
+$raskrutka_site_6 = 'Knowbaza';
+
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Академика Лебедева, дом № 12, oф. 169-170';
+
+$raskrutka_site_adress_2 = 'Санкт-Петербург, ул.Академика Лебедева, д.12'; // для Яндекса
+
+$raskrutka_site_coord = '59.962086, 30.355053'; //коориднаты на яндексе
+
+$yandex_google = '<meta name="yandex-verification" content="3e2a848a6520cb98" />
+<meta name="google-site-verification" content="9g_kdGXJJ93pKqycxRskoayroi-porC7bRaKAnOkBLo" />';
